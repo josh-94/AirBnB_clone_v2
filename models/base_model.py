d4e2ae6c704c94346431075952a191f0c61f7ece
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-04-12 11:53:43 -0500

    refactor: update BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
 
-'''File with the BaseModel Class'''
+'''File with the `BaseModel` Class'''
 
 import uuid
 from datetime import datetime
 import models
 
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+from datetime import datetime
+
+Base = declarative_base()
+
 
 class BaseModel:
     '''BaseModel Class'''
+
+    id = Column(String(60), primary_key=True)
+    created_at = Column(DateTime(), default=datetime.utcnow())
+    updated_at = Column(DateTime(), default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         '''Constructor'''
@@ -23,23 +34,26 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
-        '''Method that represents the class objects as a string'''
+        '''Method that represents the class object as a string'''
+
+        if self.__dict__['_sa_instance_state'] is not None:
+            del self.__dict__['_sa_instance_state']
+
         msg = "[{}] ({}) {}".format(self.__class__.__name__,
                                     self.id,
                                     str(self.__dict__))
         return (msg)
 
     def save(self):
-        '''updates the public instance attribute updated_at
-        with the current datetime'''
-        self.updated_at = datetime.now()
+        '''Saves the object in the database'''
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        '''returns a dictionary containing all keys/values
+        '''Returns a dictionary containing all keys/values
         of __dict__ of the instance'''
         dic = {}
         for key, value in self.__dict__.items():
@@ -49,3 +63,7 @@ class BaseModel:
                 dic[key] = value
         dic["__class__"] = self.__class__.__name__
         return (dic)
+
+    def delete(self):
+        '''Deletes the current instance'''
+        models.storage.delete(self)
