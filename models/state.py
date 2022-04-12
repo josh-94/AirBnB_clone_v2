c75f86dedf37ecdf908f72b4723a7a23ea3b3652
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-04-12 11:55:04 -0500

    chore: definition of the State model(class)

--- models/state.py
+++ models/state.py
@@ -2,9 +2,18 @@
 
 '''File with the class State'''
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     '''State Class'''
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state')
+
+    @property
+    def cities(self):
+        return (State.cities)
