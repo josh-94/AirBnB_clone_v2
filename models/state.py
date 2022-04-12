383f67e68402de287272d097e865943c1d7a4a65
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-04-12 18:42:28 -0500

    fix: validation according the type of storage

--- models/state.py
+++ models/state.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 '''File with the class State'''
-
+from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+
+storage = getenv('HBNB_TYPE_STORAGE')
 
 
 class State(BaseModel, Base):
@@ -12,8 +14,11 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state')
 
-    @property
-    def cities(self):
-        return (State.cities)
+    if storage == 'db':
+        cities = relationship('City', backref='state')
+
+    else:
+        @property
+        def cities(self):
+            return (0)
