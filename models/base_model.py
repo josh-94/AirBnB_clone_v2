fa15ae739fce55e157f44272b1b09afcd8e90c73
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-04-12 18:41:06 -0500

    fix: correct initialization when the object is created

--- models/base_model.py
+++ models/base_model.py
@@ -17,9 +17,9 @@ Base = declarative_base()
 class BaseModel:
     '''BaseModel Class'''
 
-    id = Column(String(60), primary_key=True)
-    created_at = Column(DateTime(), default=datetime.utcnow())
-    updated_at = Column(DateTime(), default=datetime.utcnow())
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime(), default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime(), default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
         '''Constructor'''
@@ -30,10 +30,10 @@ class BaseModel:
                     setattr(self, key, datetime.strptime(value, frmat))
                 elif key != "__class__":
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         '''Method that represents the class object as a string'''
