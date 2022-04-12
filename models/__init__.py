77bd0800957fab7477df5c3a561147244209cc7a
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-04-12 11:54:25 -0500

    refactor: update script

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+'''Script in which we create a unique storage
+instance for your application according of storage type we want'''
 
-'''File in which we create a unique FileStorage
-instance for your application'''
+from os import getenv
 
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
 storage.reload()
