7bf29024599243e960e26177d1e6d5c955f09b63
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-04-12 11:55:39 -0500

    chore: definition of the City model(class)

--- models/city.py
+++ models/city.py
@@ -2,10 +2,13 @@
 
 '''File with the class City'''
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     '''City Class'''
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+
+    state_id = Column(String(60), ForeignKey('states.id'))
+    name = Column(String(128), nullable=False)
