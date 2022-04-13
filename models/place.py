9336199956042180333ce8525f1a4c7b6bbf9492
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-04-13 15:34:48 -0500

    chore: definition of the Place model(class)

--- models/place.py
+++ models/place.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 
 '''File with the class Place'''
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
-from models.base_model import BaseModel
 
-
-class Place(BaseModel):
+class Place(BaseModel, Base):
     '''Place Class'''
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = 'places'
+
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True, default='NULL')
+    number_rooms = Column(Integer(), nullable=False, default=0)
+    number_bathrooms = Column(Integer(), nullable=False, default=0)
+    max_guest = Column(Integer(), nullable=False, default=0)
+    price_by_night = Column(Integer(), nullable=False, default=0)
+    latitude = Column(Float(), nullable=True, default=0.000)
+    longitude = Column(Float(), nullable=True, default=0.000)
     amenity_ids = []
