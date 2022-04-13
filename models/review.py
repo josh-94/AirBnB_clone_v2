2d36aa7ca8234a7a85e91388374bc92a724b7475
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-04-13 16:16:41 -0500

    chore: definition of the Review model(class)

--- models/review.py
+++ models/review.py
@@ -2,11 +2,13 @@
 
 '''File with the class Review'''
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     '''Review Class'''
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = 'reviews'
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    text = Column(String(1024), nullable=False)
