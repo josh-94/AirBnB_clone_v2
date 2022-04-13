8f8568a6107abff488756b39810e32dc94051d2e
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-04-12 23:22:01 -0500

    fix: initialization is in the start of the constructor

--- models/base_model.py
+++ models/base_model.py
@@ -23,6 +23,11 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         '''Constructor'''
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+
         if len(kwargs) > 0:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -30,10 +35,6 @@ class BaseModel:
                     setattr(self, key, datetime.strptime(value, frmat))
                 elif key != "__class__":
                     setattr(self, key, value)
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
 
     def __str__(self):
         '''Method that represents the class object as a string'''
