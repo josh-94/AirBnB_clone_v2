f327296c0c7c22948f35dba3492213429e203fac
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-04-13 15:38:20 -0500

    chore: added attribute(places) in the definition

--- models/city.py
+++ models/city.py
@@ -4,6 +4,7 @@
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -12,3 +13,4 @@ class City(BaseModel, Base):
 
     state_id = Column(String(60), ForeignKey('states.id'))
     name = Column(String(128), nullable=False)
+    places = relationship('Place', backref='cities')
