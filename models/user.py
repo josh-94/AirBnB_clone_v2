40af97e37a0a2a97db8e464589549ee9b4132ed9
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-04-13 21:28:03 -0500

    chore: definition of the relationships

--- models/user.py
+++ models/user.py
@@ -12,7 +12,7 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False, default="")
+    last_name = Column(String(128), nullable=False, default="")
     places = relationship('Place', backref='user')
     reviews = relationship('Review', backref='user')
