b3f25c10434904dd7f3b61ca49e007b932446363
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-04-13 21:20:12 -0500

    chore: definition of the Amenity model(class)

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 
-
 '''File with the class Amenity'''
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     '''Amenity Class'''
-    name = ""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place',
+                                   secondary='place_amenity',
+                                   back_populates='amenities')
