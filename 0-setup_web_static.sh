a7b9059d261c4047f4eb666dd7c02ea96b1c8663
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-04-28 11:27:36 -0500

    fix: the symbolic link is not a directory

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,11 +12,11 @@ sudo chown -R ubuntu:ubuntu /data
 echo "Test my configuration file" > /data/web_static/releases/test/index.html
 
 ## Creates symbolic link
-sym_link="/data/web_static/current/"
+sym_link="/data/web_static/current"
 sudo ln -sf /data/web_static/releases/test/ "$sym_link"
 
 ## Modify the Ngnix configuration file to deploy a web_static
-location_block="\n\tlocation /hbnb_static/ {\n\t\talias $sym_link;\n\t}\n"
+location_block="\n\tlocation /hbnb_static/ {\n\t\talias $sym_link/;\n\t}"
 sudo sed -i "/server_name _;/ a \\$location_block" /etc/nginx/sites-available/default
 
 sudo service nginx restart
