25f4e6ef3642b96c52c41bf18df2b3a6984e72a3
josh.94 <jeshua.cabanillas@gmail.com>
2022-05-13 12:38:35 -0500

    Add: directory and tasks

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''Script that starts a Flask web application'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def d_text_c(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<string:text>", strict_slashes=False)
+def d_text_python(text='is cool'):
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_a_number(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
