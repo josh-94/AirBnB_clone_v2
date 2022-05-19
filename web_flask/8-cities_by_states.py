da3ed74b3dd2a82454212c6fb5d35c9610afabfd
josh.94 <jeshua.cabanillas@gmail.com>
2022-05-19 11:50:53 -0500

    Add: task 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Script that starts a Flask web application'''
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def storage_close(self):
+    '''After each request you must remove the current SQLAlchemy Session '''
+    storage.close()
+
+@app.route('/cities_by_states', strict_slashes=False)
+def display_citiesBS():
+    st = storage.all(State).values()
+    return render_template('8-cities_by_states.html', st=st)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
