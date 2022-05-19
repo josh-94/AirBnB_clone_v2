1a9c943b146d54e1ff948dc18d11c5a80d7f53d6
josh.94 <jeshua.cabanillas@gmail.com>
2022-05-19 12:07:43 -0500

    update: code fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,13 +9,18 @@ app = Flask(__name__)
 
 @app.route('/cities_by_states', strict_slashes=False)
 def display_citiesBS():
-    st = storage.all(State).values()
+    """Returns a rendered html template at the /8-cities_by_states route,
+    listing all cities"""
+    st = storage.all('State').values()
     return render_template('8-cities_by_states.html', st=st)
 
 
 @app.teardown_appcontext
 def close_storage(self):
+    '''Removes the current SQLAlchemy Session'''
     storage.close()
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
