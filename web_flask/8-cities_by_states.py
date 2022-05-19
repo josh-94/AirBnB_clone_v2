a94a3f49cc5dc48c747b82c51579d00cf2828756
josh.94 <jeshua.cabanillas@gmail.com>
2022-05-19 12:00:02 -0500

    update: code fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-'''Script that starts a Flask web application'''
+'''Script that starts a Flask web application and provides cities by state'''
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 app = Flask(__name__)
 
-
-@app.teardown_appcontext
-def storage_close(self):
-    '''After each request you must remove the current SQLAlchemy Session '''
-    storage.close()
 
 @app.route('/cities_by_states', strict_slashes=False)
 def display_citiesBS():
     st = storage.all(State).values()
     return render_template('8-cities_by_states.html', st=st)
 
+
+@app.teardown_appcontext
+def close_storage(self):
+    storage.close()
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
